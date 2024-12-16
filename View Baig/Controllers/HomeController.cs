ddcb0f927c0ad5e97d638fb56eb28666a738524e
sahoon1 <siblingszahid@gmail.com>
2024-12-16 20:29:24 +0500

    work

---
+++ View Baig/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View_Baig.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            ViewBag.Message = "Message From View Bag";
+            ViewBag.Message = "Message From View Bag";
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+            string[] fruits = { "Apple", "Mango", "Banana", "Orange" };
+            ViewBag.FruitsArray = fruits;
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket",
+                "Football",
+                "Hockey",
+            };
+
+            
+
+            return View();
+
+
+        }
+    }
+}
